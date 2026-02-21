21a3385f1bf8157abdace1d7e492c091719b9f12
Eric Luo <el107@rice.edu>
2026-02-21 11:41:29 -0600

    Add files via upload

---
+++ bob/functions/api/bills.js
@@ -0,0 +1,33 @@
+import { jsonResponse } from '../_shared/congress.js';
+
+export async function onRequestGet({ request, env }) {
+  const { searchParams } = new URL(request.url);
+  const start_date = searchParams.get('start_date');
+  const end_date = searchParams.get('end_date');
+  const congress = searchParams.get('congress');
+
+  try {
+    let results;
+
+    if (start_date && end_date) {
+      ({ results } = await env.DB.prepare(`
+        SELECT DISTINCT b.*
+        FROM house_bills b
+        JOIN house_sponsorships s ON b.id = s.bill_id
+        WHERE s.date >= ? AND s.date <= ?
+      `).bind(start_date, end_date).all());
+    } else if (congress) {
+      ({ results } = await env.DB.prepare(`
+        SELECT * FROM house_bills WHERE congress = ?
+      `).bind(congress).all());
+    } else {
+      ({ results } = await env.DB.prepare(`
+        SELECT * FROM house_bills LIMIT 1000
+      `).all());
+    }
+
+    return jsonResponse(results);
+  } catch (err) {
+    return jsonResponse({ error: 'Server error', detail: err.message }, 500);
+  }
+}
